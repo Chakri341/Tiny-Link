83a48e95b528f1340e062d31fb72af6ac11b19cd
Chakradhar <chakradhar.potluru@codeverse.in>
2025-11-28 22:55:32 +0530

    type error in link

--- lib/types.ts
+++ lib/types.ts
@@ -1,15 +1,21 @@
-export type LinkItem = {
-  code: string;
-  url: string;
-  clicks: number;
-  lastClicked: Date | null;
-  createdAt: Date;
-  expiresAt: Date | null; 
-  passwordHash :string | null
-};
+import { Link } from "@prisma/client";
+
+export type LinkItem = Link;
+
 
 export interface LinksTableProps {
   initialLinks: LinkItem[];
   pageSize: number;
   initialHasMore: boolean;
 }
+
+
+// export type LinkItem = {
+//   code: string;
+//   url: string;
+//   clicks: number;
+//   lastClicked: Date | null;
+//   createdAt: Date;
+//   expiresAt: Date | null; 
+//   passwordHash :string | null
+// };
